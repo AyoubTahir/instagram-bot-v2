5785204ed4e845f1f2bb85fdb516b9f7f54472b2
AyoubTahir <ayoub.tahir.2021@gmail.com>
2022-10-06 16:01:05 +0100

    3 update

--- index.js
+++ index.js
@@ -9,21 +9,50 @@ const emailOrUsername = "";
 const accountUsername = "";
 const password = "";
 
-const linkOfLikersList = "https://www.instagram.com/p/CiadT-rACEz/liked_by/";
+const linkOfLikersList = "";
 
-const numberOfProfiles = 15;
+const numberOfPrivateProfilesToFollow = 10;
+const numberOfProfilesToCommentOn = 15; //Always follow the porfile that you comment on
 
-const minDelayBettwenComments = 3; //by minutes
-const maxDelayBettwenComments = 5; //by minutes
+const acitivateCommenting = true;
+const acitivateFollowing = true;
 
+const minDelayBettwenFollowAndComment = 60; //by seconds
+const maxDelayBettwenFollowAndComment = 180; //by seconds
+
+const minDelayBettwenUsersJustComment = 15; //by minutes
+const maxDelayBettwenUsersJustComment = 20; //by minutes
+
+const minDelayBettwenUsersJustFollow = 3; //by minutes
+const maxDelayBettwenUsersJustFollow = 5; //by minutes
+
+let hex = "🙌".codePointAt(0).toString(16);
+let emo = String.fromCodePoint("0x" + hex);
 const commentMessages = [
-  "Can you give me your feedback on this new digital budget planner? @tdigitalstudio",
+  "Can you give me your feedback on this new digital budget planner? and grab a free weekly planner " +
+    emo +
+    " you can find it in my profile",
 
-  "I would like to hear your thoughts about this new digital budget planner @tdigitalstudio",
+  "I would like to hear your thoughts about this new digital budget planner and get a free weekly planner " +
+    emo +
+    " you can find it in my bio",
 
-  "Would you mind sharing your thoughts about this new digital budget planner with me? @tdigitalstudio",
+  "Would you mind sharing your thoughts about this new digital budget planner with me? " +
+    emo +
+    " you can also grab a free weekly planner in my bio",
 
-  "What do you think of this digital budget planner?  @tdigitalstudio",
+  "What do you think of this digital budget planner? " +
+    emo +
+    " (Free weekly planner available) you can find it in my profile",
+  "Have you tried this digital budget planner? " +
+    emo +
+    " also, check out my profile for a free weekly planner",
+
+  "I was wondering if you would mind sharing your thoughts about this digital budget planner, check it out in my bio",
+
+  "In my bio, I have a link to a digital budget planner. " +
+    emo +
+    " Would you mind sharing your thoughts about it?",
 ];
 
 (async () => {
@@ -83,9 +112,17 @@ const commentMessages = [
   });
 
   let numberOfComments = 0;
+  let numberOfFollows = 0;
+  console.log(
+    "Start working on " +
+      Math.max(numberOfPrivateProfilesToFollow, numberOfProfilesToCommentOn) +
+      " profiles"
+  );
   for (
     let j = 0;
-    j < likersList.length && numberOfComments < numberOfProfiles;
+    j < likersList.length &&
+    numberOfComments <
+      Math.max(numberOfPrivateProfilesToFollow, numberOfProfilesToCommentOn);
     j++
   ) {
     // delete value before search
@@ -104,90 +141,182 @@ const commentMessages = [
           delay: 200,
         });*/
     await page.goto("https://www.instagram.com/" + likersList[j]);
-
+    let followed = false;
     try {
       //after search click in the dropdown menu
       //await page.waitForSelector("._abnx._aeul > div a");
       //await page.waitForTimeout(3000);
       //await page.click("._abnx._aeul > div a");
 
-      await page.waitForSelector(
-        "article[class='_aayp'] > div > div > div._ac7v._aang > div a",
-        getRndInteger(4000, 6000)
-      );
-      await page.waitForTimeout(3500);
-
-      //click first post
-      await page.click(
-        "article[class='_aayp'] > div > div > div._ac7v._aang > div a"
-      );
-
-      await page.waitForSelector(
-        "section[class=' _aaoe _ae5y _ae5z _ae62'] form textarea",
-        getRndInteger(4000, 6000)
-      );
-      await page.waitForTimeout(4000);
-
-      //check if already commented
-      const commented = await page.evaluate((accu) => {
-        const allComments = document.querySelectorAll(
-          "div._ae5q._ae5r._ae5s ul._a9z6._a9za ul._a9ym"
-        );
-
-        let usernames = [];
-
-        for (let i = 0; i < allComments.length; i++) {
-          usernames.push(
-            allComments[i].querySelector("h3._a9zc span a").innerText
+      if (
+        acitivateFollowing &&
+        numberOfFollows < numberOfPrivateProfilesToFollow
+      ) {
+        try {
+          await page.waitForSelector(
+            "div._ab8w._ab94._ab99._ab9f._ab9k._ab9p._abb3._abcm button._acan._acap._acas"
           );
-        }
-
-        return usernames.includes(accu);
-      }, accountUsername);
-      console.log(commented + " " + accountUsername);
-      if (!commented) {
-        await page.type(
-          "section[class=' _aaoe _ae5y _ae5z _ae62'] form textarea",
-          "@" +
-            likersList[j] +
-            " " +
-            commentMessages[Math.floor(Math.random() * commentMessages.length)],
-          {
-            delay: 200,
-          }
-        );
-        if (!testMode) {
           await page.waitForTimeout(3000);
+          //click folow
           await page.click(
-            "section[class=' _aaoe _ae5y _ae5z _ae62'] form button._acan._acao._acas"
+            "div._ab8w._ab94._ab99._ab9f._ab9k._ab9p._abb3._abcm button._acan._acap._acas"
           );
+          console.log(likersList[j] + " followed successfully");
+          followed = true;
+          numberOfFollows++;
+        } catch (err) {
+          console.log(likersList[j] + " Already followed");
+          if (!acitivateCommenting) {
+            continue;
+          }
         }
-        numberOfComments++;
-        console.log("commenting on " + likersList[j] + "done");
-      } else {
-        console.log("No comment for " + likersList[j] + "i already commented");
-        continue;
+
+        let isPrivateAccount = await page
+          .$eval(
+            "article[class='_aayp'] > div > div > div._ac7v._aang > div a",
+            () => true
+          )
+          .catch(() => false);
+
+        if (acitivateCommenting && followed && isPrivateAccount) {
+          const delayBettwenFollowAndComment = getRndInteger(
+            minDelayBettwenFollowAndComment * 1000,
+            maxDelayBettwenFollowAndComment * 1000
+          );
+          console.log(
+            "waiting " +
+              Math.round(delayBettwenFollowAndComment / 1000) +
+              "s to comment"
+          );
+          await page.waitForTimeout(delayBettwenFollowAndComment);
+        }
       }
 
-      //close post
-      await page.waitForTimeout(3000);
-      await page.click("div.x78zum5.x6s0dn4.xl56j7k.xdt5ytf svg");
-      await page.waitForTimeout(3000);
+      if (
+        acitivateCommenting &&
+        numberOfComments < numberOfProfilesToCommentOn
+      ) {
+        await page.waitForSelector(
+          "article[class='_aayp'] > div > div > div._ac7v._aang > div a",
+          getRndInteger(4000, 6000)
+        );
+        await page.waitForTimeout(3500);
+
+        //click first post
+        await page.click(
+          "article[class='_aayp'] > div > div > div._ac7v._aang > div a"
+        );
+
+        await page.waitForSelector(
+          "section[class=' _aaoe _ae5y _ae5z _ae62'] form textarea",
+          getRndInteger(4000, 6000)
+        );
+        await page.waitForTimeout(4000);
+
+        //check if already commented
+        const commented = await page.evaluate((accu) => {
+          const allComments = document.querySelectorAll(
+            "div._ae5q._ae5r._ae5s ul._a9z6._a9za ul._a9ym"
+          );
+
+          let usernames = [];
+
+          for (let i = 0; i < allComments.length; i++) {
+            usernames.push(
+              allComments[i].querySelector("h3._a9zc span a").innerText
+            );
+          }
+
+          return usernames.includes(accu);
+        }, accountUsername);
+        console.log(commented + " " + accountUsername);
+        if (!commented) {
+          let doubleCheckNeedFollow = await page
+            .$eval(
+              "div._ab8w._ab94._ab99._ab9f._ab9k._ab9p._abb3._abcm button._acan._acap._acas",
+              () => true
+            )
+            .catch(() => false);
+          if (doubleCheckNeedFollow) {
+            await page.click(
+              "div._ab8w._ab94._ab99._ab9f._ab9k._ab9p._abb3._abcm button._acan._acap._acas"
+            );
+            console.log(likersList[j] + " followed successfully");
+
+            const delayBettwenFollowAndComment = getRndInteger(
+              minDelayBettwenFollowAndComment * 1000,
+              maxDelayBettwenFollowAndComment * 1000
+            );
+            console.log(
+              "waiting " +
+                Math.round(delayBettwenFollowAndComment / 1000) +
+                "s to comment"
+            );
+            await page.waitForTimeout(delayBettwenFollowAndComment);
+          }
+
+          console.log("commenting on " + likersList[j]);
+          await page.type(
+            "section[class=' _aaoe _ae5y _ae5z _ae62'] form textarea",
+            /*"@" +
+              likersList[j] +
+              " " +*/
+            commentMessages[Math.floor(Math.random() * commentMessages.length)],
+            {
+              delay: 200,
+            }
+          );
+          if (!testMode) {
+            await page.waitForTimeout(3000);
+            await page.click(
+              "section[class=' _aaoe _ae5y _ae5z _ae62'] form button._acan._acao._acas"
+            );
+          }
+          numberOfComments++;
+          console.log("done");
+        } else {
+          console.log(
+            "No comment for " + likersList[j] + "i already commented"
+          );
+          continue;
+        }
+
+        //close post
+        await page.waitForTimeout(3000);
+        await page.click("div.x78zum5.x6s0dn4.xl56j7k.xdt5ytf svg");
+        await page.waitForTimeout(3000);
+      }
     } catch (error) {
       console.log(
-        "Can't comment on " + likersList[j] + " its private accounts"
+        "Can't comment on " + likersList[j] + " its a private account"
       );
+      if (followed) {
+        const delayAfterFollow = getRndInteger(
+          minDelayBettwenUsersJustFollow * 60 * 1000,
+          maxDelayBettwenUsersJustFollow * 60 * 1000
+        );
+
+        //wait ? minute for the next one
+        console.log(
+          "waiting " +
+            Math.round(delayAfterFollow / 1000 / 60) +
+            " min for the next user"
+        );
+        await page.waitForTimeout(delayAfterFollow);
+      }
       continue;
     }
 
     const delay = getRndInteger(
-      minDelayBettwenComments,
-      maxDelayBettwenComments
+      minDelayBettwenUsersJustComment * 60 * 1000,
+      maxDelayBettwenUsersJustComment * 60 * 1000
     );
 
     //wait ? minute for the next one
-    console.log("waiting " + delay + "min to comment on the next one");
-    await page.waitForTimeout(delay * 60 * 1000);
+    console.log(
+      "waiting " + Math.round(delay / 1000 / 60) + " min for the next user"
+    );
+    await page.waitForTimeout(delay);
   }
 
   //commentBot.commentFromListOfUsers(page, likersList, 15, commentMessages);
